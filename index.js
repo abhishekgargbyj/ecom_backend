8b8c65e6d50f155f7e4c9f96eeae32a741d9696d
Tnluser <tnluser@Administrators-MacBook-Pro.local>
2022-08-23 20:35:32 +0530

    Server setup

--- index.js
+++ index.js
@@ -23,24 +23,24 @@ app.use(bodyParser.json())
 //middleware for cookies
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello")
 })
-app.use("/login",login);
-app.use("/signup",signup);
-app.use("/refresh",require("./routes/refreshToken"));
+app.use("/login", login);
+app.use("/signup", signup);
+app.use("/refresh", require("./routes/refreshToken"));
 app.use('/logout', require('./routes/logout'));
 
 app.use(verifyJWT) //this middleware will be called for the routes mentioned below this
-app.use("/user",user);
+app.use("/user", user);
 //one more route will be added,for getAllPRoducts API for the main page 
 
 const port = process.env.PORT || 3500;
 
-app.listen(port, (error)=> {
-    if(!error){
+app.listen(port, (error) => {
+    if (!error) {
         console.log("Server is running sucessfully");
-    } 
+    }
     else
         console.log("Error is running server");
-} )
+})
