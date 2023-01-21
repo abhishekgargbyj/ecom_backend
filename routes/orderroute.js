344156086b78219e50fd5c087d024ecbd8d37410
sesurya <chakkapalli.suryateja@byjus.com>
2023-01-21 14:17:53 +0530

    Resetting the be for demo ecommerce

--- routes/orderroute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controller/ordercontroller');
-
-router.get('/getProduct', controller.getProduct);
-router.get('/getOrderDetails', controller.getOrderDetails);
-router.post('/postOrderDetails',controller.postOrderDetails);
-module.exports = router;
